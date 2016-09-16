7043dfbcdfa7c1598b9a9a109c1448955b1e5b29
shinchiro <shinchiro@users.noreply.github.com>
2016-09-16 20:40:15 +0800

    updater.bat: expand searching for 'updater.ps1' file

--- mpv-root/updater.bat
+++ mpv-root/updater.bat
@@ -1,2 +1,8 @@
 @echo OFF
-powershell -noexit -executionpolicy bypass -File .\installer\updater.ps1
+:: expand searching for 'updater.ps1' file in same folder as mpv.exe too
+if exist "%~dp0\installer\updater.ps1" (
+    set updater_script="%~dp0\installer\updater.ps1"
+) else (
+    set updater_script="%~dp0\updater.ps1"
+)
+powershell -noexit -executionpolicy bypass -File %updater_script%
