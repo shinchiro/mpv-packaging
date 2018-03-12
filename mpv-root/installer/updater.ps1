44696d9b056dede3f38145db6ce19199343f27b4
shinchiro <shinchiro@users.noreply.github.com>
2018-03-12 14:37:30 +0800

    updater.ps1: fix incompatibility when forcing TLS 1.2 on  Powershell 3

--- mpv-root/installer/updater.ps1
+++ mpv-root/installer/updater.ps1
@@ -250,7 +250,7 @@ else {
 try {
     Check-PowershellVersion
     # Sourceforge only support TLS 1.2
-    [Net.ServicePointManager]::SecurityProtocol = [System.Security.Authentication.SslProtocols] "tls, tls11, tls12"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Upgrade-Mpv
     Upgrade-Youtubedl
     Write-Host "Operation completed" -ForegroundColor Magenta
